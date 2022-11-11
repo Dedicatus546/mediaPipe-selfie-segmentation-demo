d2d714bd47fe8217be15304240e3b57d93abefb9
Dedicatus545 <1607611087@qq.com>
2022-11-11 11:55:16 +0800

    chore: 删除无关依赖

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const theme = {
-  fontColor: "#fff",
-  bgColor: "#000",
-};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
